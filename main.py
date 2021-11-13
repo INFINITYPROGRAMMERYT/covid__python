8b1a8c93466fd2e52d2f31e4277598a90410025e
INFINITYPROGRAMMERYT <infinityprogrammerofficial@gmail.com>
2021-11-13 21:27:48 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#specific state
+from covid_india import states
+print(states.getdata('Tamil Nadu'))
+
+#total cases
+from covid_india import states
+print(states.getdata('Total'))
+
+#all states
+from covid_india import states
+print(states.getdata(''))
+
